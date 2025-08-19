c0f7537d588f847d9e80d58a617ab7f5125096a3
ana :D <bruclaramagalhaes2015@gmail.com>
2025-08-19 18:27:56 -0300

    primeiro commit

---
+++ js/app.js
@@ -0,0 +1,61 @@
+function comprar() {
+    let tipo = document.getElementById('tipo-ingresso');
+    let qtd = parseInt(document.getElementById('qtd').value);
+
+    if (tipo.value == 'pista') {
+        comprarPista(qtd);
+    }
+    else if (tipo.value == 'superior') {
+        comprarSuperior(qtd);
+    }
+    else if (tipo.value == 'inferior') {
+        comprarInferior(qtd);
+    }
+}
+
+// pista
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    
+    if (qtd > qtdPista) {
+        alert("Não há ingressos disponíveis nesse setor.");
+
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert("Compra realizada com sucesso!");
+    }
+
+}
+
+// superior
+
+function comprarSuperior(qtd) {
+    let qtdSup = parseInt(document.getElementById('qtd-superior').textContent);
+    
+    if (qtd > qtdSup) {
+        alert("Não há ingressos disponíveis nesse setor.");
+
+    } else {
+        qtdSup = qtdSup - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSup;
+        alert("Compra realizada com sucesso!");
+    }
+}
+
+// inferior
+
+function comprarInferior(qtd) {
+    let qtdInf = parseInt(document.getElementById('qtd-inferior').textContent);
+    
+    if (qtd > qtdInf) {
+        alert("Não há ingressos disponíveis nesse setor.");
+
+    } else {
+        qtdInf = qtdInf - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInf;
+        alert("Compra realizada com sucesso!");
+    }
+}
+
